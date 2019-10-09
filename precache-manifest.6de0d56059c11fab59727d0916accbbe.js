04f119e6769ce2b0536a085c682129d9835efe1c
arvi <arvikonoha@gmail.com>
2019-10-10 05:09:59 +0530

    Updates

---
+++ precache-manifest.6de0d56059c11fab59727d0916accbbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "904a9b57eaa1257b042a26abae272016",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e783ac0a46c52a5a8680",
+    "url": "/monsters-rolodex/static/css/main.651e3e5a.chunk.css"
+  },
+  {
+    "revision": "ca903edc950d52328219",
+    "url": "/monsters-rolodex/static/js/2.e124cbd2.chunk.js"
+  },
+  {
+    "revision": "e783ac0a46c52a5a8680",
+    "url": "/monsters-rolodex/static/js/main.8b533a6a.chunk.js"
+  },
+  {
+    "revision": "d71975295fe45a6cbab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.4686dfee.js"
+  }
+]);
